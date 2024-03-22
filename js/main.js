84cc8fa88f023b0ea11dea6eed22b5176d5dc080
Fedeplaysd <fedpedrazaimb@gmail.com>
2024-03-22 11:13:46 -0300

    about me added, fixed open blank

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-function enviarMensajeWhatsApp() {
-  window.location.href = "https://wa.me/+543571515711100?text=Gracias por comunicarte! En breve atendere tu mensaje.";
+function ingresarPaginaAboutMe() {
+window.location.href = "pages/aboutMe.html";
 }
 function ingresarPaginaLinkedIn() {
   window.location.href = "https://www.linkedin.com/in/federico-pedraza-arg/"; 
 }
 function ingresarPaginaGitHub() {
   window.location.href = "https://github.com/Fedeplaysd"; 
+}
+function volverInicio() {
+  window.location.href = "../index.html"; 
 }
