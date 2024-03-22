477d00c617c3b0192d7ab2a33dec800afa06bd2f
Fedeplaysd <fedpedrazaimb@gmail.com>
2024-03-22 10:31:31 -0300

    v2

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function enviarMensajeWhatsApp() {
+  window.location.href = "https://wa.me/+543571515711100?text=Gracias por comunicarte! En breve atendere tu mensaje.";
+}
+function ingresarPaginaLinkedIn() {
+  window.location.href = "https://www.linkedin.com/in/federico-pedraza-arg/"; 
+}
+function ingresarPaginaGitHub() {
+  window.location.href = "https://github.com/Fedeplaysd"; 
+}
